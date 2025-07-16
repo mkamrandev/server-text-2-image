47a1d7eb3eb422bb90661ad779637ba6df326fde
Muhammad Kamran <contactwithmr.kamran@gmail.com>
2025-07-16 18:05:08 +0500

    Added CORS fix for Netlify frontend

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ const app = express()
 
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: "https://inkimage.netlify.app",
+  credentials: true
+}));
 await connectDB()
 
 app.use('/api/user', userRouter)
